dcb1bf725aa28d4906fd01dfb6d982b7bf60cfd3
곽경희 <82927462+kwakkyunghee@users.noreply.github.com>
2022-11-10 12:40:29 +0900

    모델의 구조 확립

--- script.js
+++ script.js
@@ -12,7 +12,23 @@ async function getData() {
         .filter(car => (car.mpg != null && car.horsepower != null));
 
     return cleaned;
+
 }
+
+
+function createModel() {
+    // Create a sequential model
+    const model = tf.sequential();
+
+    // Add a single input layer
+    model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true}));
+
+    // Add an output layer
+    model.add(tf.layers.dense({units: 1, useBias: true}));
+
+    return model;
+}
+
 async function run() {
     // Load and plot the original input data that we are going to train on.
     const data = await getData();
@@ -32,6 +48,9 @@ async function run() {
     );
 
     // More code will be added below
+    // Create the model
+    const model = createModel();
+    tfvis.show.modelSummary({name: 'Model Summary'}, model);
 }
 
 document.addEventListener('DOMContentLoaded', run);
